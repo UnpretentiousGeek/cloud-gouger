d1455005781e713c205b24a5f6b94e4f5b47082a
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-10-22 19:05:42 +0000

    erwge

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
 if "img" in st.session_state:
-    st.image(st.session_state.img)
+    col1, col2 = st.columns([3, 1])
+    col2.st.image(st.session_state.img)
 
 @st.dialog("Cast your vote")
 def vote():
+    
     enable = st.checkbox("Enable camera")
     picture = st.camera_input("Take a picture", disabled=not enable)
     if picture:
