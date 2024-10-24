e542a8068be95d20765da6c54b7d35598c43d762
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-10-24 03:42:48 +0000

    stbd

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,11 @@ uploaded_file = st.file_uploader(
 image_path = "k2xnp9yctuw91.jpg"
 
 # Getting the base64 string
-base64_image = encode_image(uploaded_file.getvalue())
+
 
 st.session_state.client = OpenAI(api_key=st.secrets['openai_key'])
 if uploaded_file:
+    base64_image = encode_image(uploaded_file)
     stream = st.session_state.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
