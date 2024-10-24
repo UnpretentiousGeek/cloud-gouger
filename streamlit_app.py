e9e4416cc4ef85c1bf878cda96b7a13ec26476ac
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-10-24 03:41:53 +0000

    ytdnfg

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,32 +6,35 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
   
+uploaded_file = st.file_uploader(
+    "Upload a document")
+  
 image_path = "k2xnp9yctuw91.jpg"
 
 # Getting the base64 string
-base64_image = encode_image(image_path)
+base64_image = encode_image(uploaded_file)
 
 st.session_state.client = OpenAI(api_key=st.secrets['openai_key'])
-
-stream = st.session_state.client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "What’s in this image?"},
+if uploaded_file:
+    stream = st.session_state.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
         {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}",
-            "detail": "high"
-          },
-        },
-      ],
-    }
-  ],
-        stream=True
-    )
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "What’s in this image?"},
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}",
+                "detail": "high"
+            },
+            },
+        ],
+        }
+    ],
+            stream=True
+        )
 
-st.write_stream(stream)
+    st.write_stream(stream)
 
